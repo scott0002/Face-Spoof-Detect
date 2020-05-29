e83e471410490266aff9ad6432e132ec34854e18
scott0002 <scott890719@gmail.com>
2020-05-29 19:03:46 +0800

    Update test.py

--- test.py
+++ test.py
@@ -22,24 +22,45 @@ def load_image():
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ]),
     }
-    data_dir = "..\LCC_FASD"
-    image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
-                                            data_transforms[x])
-                    for x in ['evaluation']}
-    dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,
+    data_dir = "LCC_FASD"
+    image_datasets =datasets.ImageFolder(os.path.join(data_dir, 'evaluation'),
+                                            data_transforms['evaluation'])
+    dataloaders = torch.utils.data.DataLoader(image_datasets, batch_size=4,
                                                 shuffle=True, num_workers=4)
-                for x in ['evaluation']}
-    dataset_sizes = {x: len(image_datasets[x]) for x in ['evaluation']}
-
+    dataset_sizes = len(image_datasets) 
+    class_names = image_datasets.classes
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")    
     return dataset_sizes, device, class_names, dataloaders
 
 def run_test():
+    dataset_sizes, device, class_names, dataloaders=load_image()
     model = models.resnet18(pretrained=True)
-    model.load_state_dict(torch.load(model))
+    num_ftrs = model.fc.in_features
+    # Here the size of each output sample is set to 2.
+    # Alternatively, it can be generalized to nn.Linear(num_ftrs, len(class_names)).
+    model.fc = nn.Linear(num_ftrs, 2)
+
+    model = model.to(device)
+    model.load_state_dict(torch.load('model.pkl'))
     running_loss = 0.0
     running_corrects = 0
     model.eval()
+    #phase=['evalution']
+    for inputs, labels in dataloaders:
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+    # statistics
+    criterion = nn.CrossEntropyLoss()
+    outputs = model(inputs)
+    _, preds = torch.max(outputs, 1)
+    loss = criterion(outputs, labels)
+    running_loss += loss.item() * inputs.size(0)
+    running_corrects += torch.sum(preds == labels.data)
+
+    loss = running_loss / dataset_sizes
+    acc = running_corrects.double() / dataset_sizes
+
+    print('{} Loss: {:.4f} Acc: {:.4f}'.format('evaluation', loss, acc))
 
 if __name__ == '__main__':
     run_test()
